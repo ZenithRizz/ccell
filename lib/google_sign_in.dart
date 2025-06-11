4a6b0eaf4917b7e737a1cdad78e12eb6e85190b3
Praneel Dev <praneeldev49@gmail.com>
2025-06-11 21:41:02 +0530

    Initial commit

---
+++ lib/google_sign_in.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+Future<void> signInWithCollegeGoogleAccount(BuildContext context) async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+      try {
+        await googleSignIn.signOut();
+        final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+
+        if (googleUser == null) {
+          //user cancelled sign in or process terminated or smth
+          return;
+        }
+
+        final String email = googleUser.email;
+        if (!email.endsWith('@lnmiit.ac.in')) {
+          await googleSignIn.signOut();
+
+          await showDialog(
+            context: context,
+            builder: (BuildContext dialogContext) {
+              return AlertDialog(
+                backgroundColor: Color.fromARGB(255, 245, 245, 245),
+                title: Text('Used an Invalid Email'),
+
+                content: Text('Please use your LNMIIT Email ID to Sign In.'),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(dialogContext).pop();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 239, 210, 210),
+                      side: BorderSide(
+                        color: Color.fromARGB(255, 86, 3, 3),
+                        width: 2,
+                      )
+                    ),
+                    child:  Text(
+                        'Retry',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 86, 3, 3),
+                      )
+
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+
+          throw Exception('Use your Official College Email ID to Sign In.');
+        }
+
+        final GoogleSignInAuthentication googleAuth = await googleUser
+            .authentication;
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+
+        await FirebaseAuth.instance.signInWithCredential(credential);
+        print('Sign-in Successful');
+
+
+
+        //add homescreen navigation here
+
+    } catch (e) {
+    print('Sign-in failed: $e');
+    return;
+    }
+
+    }
+
+
---
+++ lib/login_page.dart
@@ -0,0 +1,101 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:login_page/google_sign_in.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+class LoginPage extends StatelessWidget{
+  const LoginPage({super.key});
+
+  @override
+  Widget build(context){
+
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+
+
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Color.fromARGB(255, 143, 0, 34),
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Stack(
+                  children: [
+                    Image.asset(
+                      'assets/images/Frame.png',
+                      width: double.infinity,
+                      fit: BoxFit.fitWidth, // Ensures full width without cropping height
+                    ),
+                    Positioned(
+                      top: 80, // Adjust this based on how low you want the logo
+                      left: 0,
+                      right: 0,
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: Image.asset(
+                          'assets/images/ccell_logo.png',
+                          height: 400, // You can change size here
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              Text(
+                  'Welcome!',
+                  style:
+                  TextStyle(
+                    fontSize: 40,
+                  )
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Text(
+                  'LOGIN WITH YOUR COLLEGE ACCOUNT',
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                )
+                ),
+
+                SizedBox(
+                  height: 8,
+                ),
+              GestureDetector(
+                onTap: () {
+                  signInWithCollegeGoogleAccount(context);
+                },
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  //padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: Image.asset(
+                      'assets/images/google_logo.png',
+                    height: 50,
+                    fit: BoxFit.contain
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.black,
+                ),
+                child: Text('Unable to Login?'),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
